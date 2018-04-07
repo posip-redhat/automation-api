b1eeac90d8decc130b1a77dee639fcf199094160
Phil Osip <35852169+posip-redhat@users.noreply.github.com>
2018-04-07 06:00:59 -0500

    Comment out CORS

--- src/main/java/com/rhc/automation/api/ApiOriginFilter.java
+++ src/main/java/com/rhc/automation/api/ApiOriginFilter.java
@@ -11,7 +11,7 @@ public class ApiOriginFilter implements javax.servlet.Filter {
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
-		res.addHeader("Access-Control-Allow-Origin", "*");
+		//res.addHeader("Access-Control-Allow-Origin", "*");
 		res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
 		res.addHeader("Access-Control-Allow-Headers", "Content-Type");
 		chain.doFilter(request, response);
