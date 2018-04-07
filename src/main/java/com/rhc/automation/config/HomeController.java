4e94ede9ba6489eea811d251d16ae3b8e8f84a88
Phil Osip <35852169+posip-redhat@users.noreply.github.com>
2018-04-07 05:52:18 -0500

    Updated imports

--- src/main/java/com/rhc/automation/config/HomeController.java
+++ src/main/java/com/rhc/automation/config/HomeController.java
@@ -2,6 +2,7 @@ package com.rhc.automation.config;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Home redirection to swagger api documentation 
